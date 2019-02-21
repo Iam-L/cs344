6cb11e3aba7462da23777a4d80572a340f4c7147
Joseph Jinn <sorcere666@gmail.com>
2019-02-21 13:13:48 -0500

    Progress made on homework 1, I think...

--- Homeworks/Homework1/course_scheduling.py
+++ Homeworks/Homework1/course_scheduling.py
@@ -1,121 +1,143 @@
 """
-This uses a pre-built problem specification for the
-Zebra puzzle taken from AIMA's csp.py. See:
+Course: CS 344 - Artificial Intelligence
+Instructor: Professor VanderLinden
+Name: Joseph Jinn
+Date: 2-20-19
 
-    https://en.wikipedia.org/wiki/Zebra_Puzzle
+Homework 1 - Problem Solving
+Course Scheduling Constraint Satisfaction Problem
 
-@author: kvlinden
-@version 14feb2013
+Note to self:
+
+TODO - Ask Professor VanderLinden about the parts I don't quite understand.
+
+FIXME - Not working at the moment.
 """
+
+############################################################################################
+############################################################################################
+
+# Time how long each algorithm takes.
 import time
 
-from csp import Zebra, min_conflicts, backtracking_search, AC3
-from search import depth_first_graph_search
+# Import everything because I'm lazy and inefficient. ;D
+import csp
+import search
+
+
+############################################################################################
+############################################################################################
+
+# Define a course scheduling formulation.
+def courses():
+    # Defines the variables.
+    Courses = 'cs108 cs112 cs212 cs214 cs232 cs262 cs344'.split()
+    Faculty = 'adams vanderlinden plantinga wieringa norman'.split()
+    TimeSlots = 'mwf8:00-8:50 mwf9:00-9:50 mwf10:30-11:20 tth11:30-12:20 tth12:30-1:20'.split()
+    Classrooms = 'nh253 sb382'.split()
+    variables = Courses + Faculty + TimeSlots + Classrooms
+
+    # Defines the domain.
+    domains = {}
+    for var in variables:
+        domains[var] = list(range(1, 7))
+    domains['cs108'] = [1]
+    domains['cs112'] = [2]
+    domains['cs212'] = [3]
+    domains['cs214'] = [4]
+    domains['cs232'] = [5]
+    domains['cs262'] = [6]
+    domains['cs344'] = [7]
+
+    # Debug.
+    for d in domains:
+        print("Domain value: " + d)
+
+    # Define the values associated with variables.
+    neighbors = csp.parse_neighbors("""adams: cs112;
+                adams: cs214; vanderlinden: cs344; wieringa: cs108;
+                plantinga: cs212; norman: cs232; vanderlinden: cs262""", variables)
+
+    # Debug.
+    for n in neighbors:
+        print("Neighbors Value:" + n)
+
+    for types in [Courses, Faculty, TimeSlots, Classrooms]:
+        for A in types:
+            for B in types:
+                if A != B:
+                    if B not in neighbors[A]:
+                        neighbors[A].append(B)
+                    if A not in neighbors[B]:
+                        neighbors[B].append(A)
+
+    # Define the constraints on the variables.
+    def scheduling_constraint(A, a, B, b, recurse=0):
+        same = (a == b)
+        next_to = abs(a - b) == 1
+        if A == 'adams' and B == 'cs112':
+            return same
+        if A == 'adams' and B == 'cs214':
+            return same
+        if A == 'vanderlinden' and B == 'cs344':
+            return same
+        if A == 'wieringa' and B == 'cs108':
+            return same
+        if A == 'plantinga' and B == 'cs212':
+            return same
+        if A == 'norman' and B == 'cs232':
+            return same
+        if A == 'vanderlinden' and B == 'cs262':
+            return same
+        if recurse == 0:
+            return scheduling_constraint(B, b, A, a, 1)
+        if ((A in Courses and B in Courses) or
+                (A in Faculty and B in Faculty) or
+                (A in TimeSlots and B in TimeSlots) or
+                (A in Classrooms and B in Classrooms)):
+            return not same
+        raise Exception('error')
+
+    return csp.CSP(variables, domains, neighbors, scheduling_constraint)
+
+
+############################################################################################
+############################################################################################
+
+# Assign problem definition to variable.
+problem = courses()
+
+""" Select a AIMA search algorithm to use to solve the problem and time its runtime. """
+startTime = time.time()
+
+# result = search.depth_first_graph_search(problem)
+# result = csp.AC3(problem)
+# result = csp.backtracking_search(problem)
+result = csp.min_conflicts(problem, max_steps=1000)
+
+endTime = time.time()
+
+
+""" A CSP solution printer copied from csp.py. """
 
 
 def print_solution(result):
-    """A CSP solution printer copied from csp.py."""
     for h in range(1, 6):
         print('House', h)
         for (var, val) in result.items():
-            if val == h: print('\t', var)
+            if val == h:
+                print('\t', var)
 
 
-puzzle = Courses()
+""" Print the solution. """
 
-# result = depth_first_graph_search(puzzle)
-# result = AC3(puzzle)
-result = backtracking_search(puzzle)
-# result = min_conflicts(puzzle, max_steps=1000)
-
-if puzzle.goal_test(puzzle.infer_assignment()):
+if problem.goal_test(problem.infer_assignment()):
     print("Solution:\n")
     print_solution(result)
 else:
     print("failed...")
-    print(puzzle.curr_domains)
-    puzzle.display(puzzle.infer_assignment())
+    print(problem.curr_domains)
+    problem.display(problem.infer_assignment())
 
-# The Zebra Puzzle
-
-
-def Courses():
-
-
-
-# def Zebra():
-#     """Return an instance of the Zebra Puzzle."""
-#     Colors = 'Red Yellow Blue Green Ivory'.split()
-#     Pets = 'Dog Fox Snails Horse Zebra'.split()
-#     Drinks = 'OJ Tea Coffee Milk Water'.split()
-#     Countries = 'Englishman Spaniard Norwegian Ukranian Japanese'.split()
-#     Smokes = 'Kools Chesterfields Winston LuckyStrike Parliaments'.split()
-#     variables = Colors + Pets + Drinks + Countries + Smokes
-#     domains = {}
-#     for var in variables:
-#         domains[var] = list(range(1, 6))
-#     domains['Norwegian'] = [1]
-#     domains['Milk'] = [3]
-#     neighbors = parse_neighbors("""Englishman: Red;
-#                 Spaniard: Dog; Kools: Yellow; Chesterfields: Fox;
-#                 Norwegian: Blue; Winston: Snails; LuckyStrike: OJ;
-#                 Ukranian: Tea; Japanese: Parliaments; Kools: Horse;
-#                 Coffee: Green; Green: Ivory""", variables)
-#     for type in [Colors, Pets, Drinks, Countries, Smokes]:
-#         for A in type:
-#             for B in type:
-#                 if A != B:
-#                     if B not in neighbors[A]:
-#                         neighbors[A].append(B)
-#                     if A not in neighbors[B]:
-#                         neighbors[B].append(A)
-#
-#     def zebra_constraint(A, a, B, b, recurse=0):
-#         same = (a == b)
-#         next_to = abs(a - b) == 1
-#         if A == 'Englishman' and B == 'Red':
-#             return same
-#         if A == 'Spaniard' and B == 'Dog':
-#             return same
-#         if A == 'Chesterfields' and B == 'Fox':
-#             return next_to
-#         if A == 'Norwegian' and B == 'Blue':
-#             return next_to
-#         if A == 'Kools' and B == 'Yellow':
-#             return same
-#         if A == 'Winston' and B == 'Snails':
-#             return same
-#         if A == 'LuckyStrike' and B == 'OJ':
-#             return same
-#         if A == 'Ukranian' and B == 'Tea':
-#             return same
-#         if A == 'Japanese' and B == 'Parliaments':
-#             return same
-#         if A == 'Kools' and B == 'Horse':
-#             return next_to
-#         if A == 'Coffee' and B == 'Green':
-#             return same
-#         if A == 'Green' and B == 'Ivory':
-#             return a - 1 == b
-#         if recurse == 0:
-#             return zebra_constraint(B, b, A, a, 1)
-#         if ((A in Colors and B in Colors) or
-#                 (A in Pets and B in Pets) or
-#                 (A in Drinks and B in Drinks) or
-#                 (A in Countries and B in Countries) or
-#                 (A in Smokes and B in Smokes)):
-#             return not same
-#         raise Exception('error')
-#     return CSP(variables, domains, neighbors, zebra_constraint)
-
-
-# def solve_zebra(algorithm=min_conflicts, **args):
-#     z = Zebra()
-#     ans = algorithm(z, **args)
-#     for h in range(1, 6):
-#         print('House', h, end=' ')
-#         for (var, val) in ans.items():
-#             if val == h:
-#                 print(var, end=' ')
-#         print()
-#     return ans['Zebra'], ans['Water'], z.nassigns, ans
+############################################################################################
+############################################################################################
--- Homeworks/Homework1/traveling_salesman.py
+++ Homeworks/Homework1/traveling_salesman.py
@@ -1,104 +1,106 @@
 """
-This implements a local-search version of N-Queens for AIMA-Python.
-It copies AIMA's goal_test and conflict methods, modifying the others
-to support a complete-state, value-maximization formulation.
+Course: CS 344 - Artificial Intelligence
+Instructor: Professor VanderLinden
+Name: Joseph Jinn
+Date: 2-20-19
 
-@author: kvlinden
-@version 19dec2012
+Homework 1 - Problem Solving
+Traveling Salesman Local Search Problem
+
+Note to self:
+
+FIXME - don't comprehend where to even start...
+
 """
+
+############################################################################################
+############################################################################################
+
+# Time how long each algorithm takes.
+import time
+
+import random
+
+# Import everything because I'm lazy and inefficient. ;D
+import csp
+import search
+
+############################################################################################
+############################################################################################
+
 from search import Problem, hill_climbing, simulated_annealing, exp_schedule
 import itertools
 import math
 
+
+# Define the traveling salesman problem.
+from utils import distance
+
+
 class Salesman(Problem):
 
-    """An implementation of NQueens for local search. This is a complete-state
-    reformulation of the partial-state AIMA-Python version.
+    """The problem of searching a graph from one node to another."""
 
-    State representation:
-        [q1, q2, ..., qn] gives the row for each of the n queens.
-    Move representation:
-        [column, row]: Move the queen in the given column to the given row.
-    """
+    def __init__(self, initial, goal, graph):
+        Problem.__init__(self, initial, goal)
+        self.graph = graph
 
-    def __init__(self, n, initial):
-        self.n = n
-        self.initial = initial
+    def actions(self, A):
+        """The actions at a graph node are just its neighbors."""
+        return list(self.graph.get(A).keys())
 
-    def actions(self, state):
-        """Actions move the queen in each column to any of the other,
-        unfilled row spots.
-        """
-        actions = []
-        for column in range(self.n):
-            for row in [x for x in range(self.n) if x != state[column]]:
-                actions.append([column, row])
-        return actions
+    def result(self, state, action):
+        """The result of going to a neighbor is just that neighbor."""
+        return action
 
-    def result(self, state, move):
-        """Makes the given move on a copy of the given state."""
-        new_state = state[:]
-        new_state[move[0]] = move[1]
-        return new_state
+    def path_cost(self, cost_so_far, A, action, B):
+        return cost_so_far + (self.graph.get(A, B) or search.infinity)
 
-    def goal_test(self, state):
-        """Check to see if there are no conflicts."""
-        return not any(self.conflicted(state, state[col], col)
-                       for col in range(len(state)))
+    def find_min_edge(self):
+        """Find minimum value of edges."""
+        m = search.infinity
+        for d in self.graph.graph_dict.values():
+            local_min = min(d.values())
+            m = min(m, local_min)
 
-    def conflicted(self, state, row, col):
-        """Check to see if placing a queen at (row, col) would conflict with
-        anything.
-        """
-        return any(self.conflict(row, col, state[c], c)
-                   for c in range(col))
+        return m
 
-    def conflict(self, row1, col1, row2, col2):
-        """Check to see if two queens placed in (row1, col1) and (row2, col2)
-        respectively would conflict with each other.
-        """
-        return (row1 == row2  # # same row
-                or col1 == col2  # # same column
-                or row1 - col1 == row2 - col2  # # same \ diagonal
-                or row1 + col1 == row2 + col2)  # # same / diagonal
+    def h(self, node):
+        """h function is straight-line distance from a node's state to goal."""
+        locs = getattr(self.graph, 'locations', None)
+        if locs:
+            if type(node) is str:
+                return int(distance(locs[node], locs[self.goal]))
 
-    def value(self, state):
-        """This method computes a value of given state based on the number of
-        conflicting pairs of queens. It doesn't follow AIMA-Python's NQueens
-        gradient-descent formulation; instead, it counts the number of
-        non-conflicting pairs (e.g., top score: 28 for a 8-queen problem;
-        worst score: 0).
-        """
-
-        # Start with the highest possible score (n combined 2).
-        value = math.factorial(self.n) / (2 * math.factorial(self.n - 2))
-
-        # Loop through all pairs, subtracting one for every conflicted pair.
-        for pair in itertools.combinations(range(self.n), 2):
-            if self.conflict(state[pair[0]], pair[0], state[pair[1]], pair[1]):
-                value -= 1
-
-        return value
+            return int(distance(locs[node.state], locs[self.goal]))
+        else:
+            return search.infinity
 
 
+# Executes the problem and finds a solution, if any.
 if __name__ == '__main__':
-    # Set the board size.
-    n = 8
 
-    # Initialize the board with all queens in the first row.
-    board = [0] * n
-    print('Start:    ' + str(board))
+    # Generate a random number to be used as distance between cities.
+    randomInt = random.randint(0, 100)
 
-    # Initialize the NQueens problem
-    p = Salesman(n, board)
-    print('Value:    ' + str(p.value(board)))
+    # Create simple map of cities to travel to.
+    cities_map = search.UndirectedGraph(dict(
+        CityA=dict(CityB=randomInt, CityC=randomInt, CityD=randomInt),
+        CityB=dict(CityC=randomInt, CityD=randomInt),
+        CityC=dict(CityD=randomInt)))
+
+    # Debug. - need to add/find methods to print out nodes and connections.
+    print("Map: " + str(cities_map))
+
+    # Initialize the Traveling Salesman Problem.
+    travel = Salesman(cities_map.nodes[0], cities_map.nodes[0], cities_map)
 
     # Solve the problem using hill climbing.
-    hill_solution = hill_climbing(p)
+    hill_solution = hill_climbing(travel)
     print('Hill-climbing:')
     print('\tSolution: ' + str(hill_solution))
-    print('\tValue:    ' + str(p.value(hill_solution)))
-    print('\tGoal?     ' + str(p.goal_test(hill_solution)))
+    print('\tValue:    ' + str(travel.value(hill_solution)))
+    print('\tGoal?     ' + str(travel.goal_test(hill_solution)))
 
     # Solve the problem using simulated annealing.
     annealing_solution = simulated_annealing(
@@ -107,5 +109,5 @@ if __name__ == '__main__':
     )
     print('Simulated annealing:')
     print('\tSolution: ' + str(annealing_solution))
-    print('\tValue:    ' + str(p.value(annealing_solution)))
-    print('\tGoal?     ' + str(p.goal_test(annealing_solution)))
+    print('\tValue:    ' + str(travel.value(annealing_solution)))
+    print('\tGoal?     ' + str(travel.goal_test(annealing_solution)))
