54aa5356ba0267d6d1393fa2077e59f06df0cc1e
Joseph Jinn <sorcere666@gmail.com>
2019-02-21 22:14:34 -0500

    Homework - warmer but not there yet.

--- Homeworks/Homework1/course_scheduling.py
+++ Homeworks/Homework1/course_scheduling.py
@@ -7,11 +7,10 @@ Date: 2-20-19
 Homework 1 - Problem Solving
 Course Scheduling Constraint Satisfaction Problem
 
-Note to self:
+Notes:
 
-TODO - Ask Professor VanderLinden about the parts I don't quite understand.
+None at the moment.
 
-FIXME - Not working at the moment.
 """
 
 ############################################################################################
@@ -24,81 +23,93 @@ import time
 import csp
 import search
 
+import time
 
 ############################################################################################
 ############################################################################################
+
+# Turn debugging on or off.
+debug = False
+
 
 # Define a course scheduling formulation.
 def courses():
-    # Defines the variables.
+    # Defines the variables and values.
     Courses = 'cs108 cs112 cs212 cs214 cs232 cs262 cs344'.split()
     Faculty = 'adams vanderlinden plantinga wieringa norman'.split()
     TimeSlots = 'mwf8:00-8:50 mwf9:00-9:50 mwf10:30-11:20 tth11:30-12:20 tth12:30-1:20'.split()
     Classrooms = 'nh253 sb382'.split()
-    variables = Courses + Faculty + TimeSlots + Classrooms
+
+    if debug:
+        # Debug statements.
+        print('Courses list:' + str(Courses))
+        print('Faculty list:' + str(Faculty))
+        print('TimeSlots list:' + str(TimeSlots))
+        print('Classrooms list:' + str(Classrooms))
+
+    variables = Courses
+    values = Faculty + TimeSlots + Classrooms
+
+    if debug:
+        # Debug statements.
+        print('My variables: ' + str(variables))
+        print('My values: ' + str(values))
 
     # Defines the domain.
-    domains = {}
+    domain = {}
     for var in variables:
-        domains[var] = list(range(1, 7))
-    domains['cs108'] = [1]
-    domains['cs112'] = [2]
-    domains['cs212'] = [3]
-    domains['cs214'] = [4]
-    domains['cs232'] = [5]
-    domains['cs262'] = [6]
-    domains['cs344'] = [7]
+        domain[var] = values
 
-    # Debug.
-    for d in domains:
-        print("Domain value: " + d)
+    if debug:
+        # Debug statements.
+        for key, value in domain.items():
+            print("My domain key: " + key)
+            print("My domain values: " + str(value))
 
-    # Define the values associated with variables.
-    neighbors = csp.parse_neighbors("""adams: cs112;
-                adams: cs214; vanderlinden: cs344; wieringa: cs108;
-                plantinga: cs212; norman: cs232; vanderlinden: cs262""", variables)
+    # Define neighbors of each variable.
+    neighbors = csp.parse_neighbors("""cs108: cs112 cs212 cs214 cs232 cs262 cs344;
+                cs112: cs108 cs212 cs214 cs232 cs262 cs344; 
+                cs212: cs108 cs112 cs214 cs232 cs262 cs344; 
+                cs214: cs108 cs112 cs212 cs232 cs262 cs344;
+                cs232: cs108 cs112 cs212 cs214 cs262 cs344; 
+                cs262: cs108 cs112 cs212 cs214 cs232 cs344; 
+                cs344: cs108 cs112 cs212 cs214 cs232 cs262""")
 
-    # Debug.
-    for n in neighbors:
-        print("Neighbors Value:" + n)
+    if debug:
+        # Debug statements.
+        for key, value in neighbors.items():
+            print("Neighbors Key:" + key)
+            print("Neighbors Values: " + str(value))
 
-    for types in [Courses, Faculty, TimeSlots, Classrooms]:
-        for A in types:
-            for B in types:
-                if A != B:
-                    if B not in neighbors[A]:
-                        neighbors[A].append(B)
-                    if A not in neighbors[B]:
-                        neighbors[B].append(A)
+    """
+        The constraints are that:
+            each course should be offered exactly once by the assigned faculty member.
+            a faculty member can only teach one thing at a time.
+            a room can only have one class at each time.
+    """
 
     # Define the constraints on the variables.
-    def scheduling_constraint(A, a, B, b, recurse=0):
-        same = (a == b)
-        next_to = abs(a - b) == 1
-        if A == 'adams' and B == 'cs112':
-            return same
-        if A == 'adams' and B == 'cs214':
-            return same
-        if A == 'vanderlinden' and B == 'cs344':
-            return same
-        if A == 'wieringa' and B == 'cs108':
-            return same
-        if A == 'plantinga' and B == 'cs212':
-            return same
-        if A == 'norman' and B == 'cs232':
-            return same
-        if A == 'vanderlinden' and B == 'cs262':
-            return same
-        if recurse == 0:
-            return scheduling_constraint(B, b, A, a, 1)
-        if ((A in Courses and B in Courses) or
-                (A in Faculty and B in Faculty) or
-                (A in TimeSlots and B in TimeSlots) or
-                (A in Classrooms and B in Classrooms)):
-            return not same
-        raise Exception('error')
+    # FIXME - I'm obviously not understanding how this is supposed to work.
+    def scheduling_constraint(A, a, B, b):
 
-    return csp.CSP(variables, domains, neighbors, scheduling_constraint)
+        # Fail if in the same room at the same time.
+        for myRoom in Classrooms:
+            if A == myRoom and B == myRoom:
+                for myTime in TimeSlots:
+                    if A == myTime and B == myTime:
+                        return False
+
+        # Fail if the same faculty at the same time.
+        for myTeacher in Faculty:
+            if A == myTeacher and B == myTeacher:
+                for myTime in TimeSlots:
+                    if A == myTime and B == myTime:
+                        return False
+
+        return True
+        # raise Exception('error')
+
+    return csp.CSP(variables, domain, neighbors, scheduling_constraint)
 
 
 ############################################################################################
@@ -117,14 +128,13 @@ result = csp.min_conflicts(problem, max_steps=1000)
 
 endTime = time.time()
 
-
 """ A CSP solution printer copied from csp.py. """
 
 
-def print_solution(result):
+def print_solution(my_results):
     for h in range(1, 6):
         print('House', h)
-        for (var, val) in result.items():
+        for (var, val) in my_results.items():
             if val == h:
                 print('\t', var)
 
@@ -138,6 +148,8 @@ else:
     print("failed...")
     print(problem.curr_domains)
     problem.display(problem.infer_assignment())
+
+    print("Time taken to execute algorithm: " + str(endTime - startTime))
 
 ############################################################################################
 ############################################################################################
--- Homeworks/Homework1/traveling_salesman.py
+++ Homeworks/Homework1/traveling_salesman.py
@@ -9,7 +9,7 @@ Traveling Salesman Local Search Problem
 
 Note to self:
 
-FIXME - don't comprehend where to even start...
+FIXME - errors galore!
 
 """
 
@@ -18,96 +18,122 @@ FIXME - don't comprehend where to even start...
 
 # Time how long each algorithm takes.
 import time
-
+# Random number generator.
 import random
 
-# Import everything because I'm lazy and inefficient. ;D
-import csp
-import search
-
-############################################################################################
-############################################################################################
-
+# Import the search algorithms we will use, cooling function, etc.
 from search import Problem, hill_climbing, simulated_annealing, exp_schedule
-import itertools
-import math
 
+############################################################################################
+############################################################################################
 
-# Define the traveling salesman problem.
-from utils import distance
+"""
+Defines the Traveling Salesman problem by modifying queens.py from u02local directory.
+"""
 
 
 class Salesman(Problem):
+    """ Initalize class variables. """
 
-    """The problem of searching a graph from one node to another."""
+    def __init__(self, distances, city):
+        self.distances = distances
+        # Note: must have self.initial as defined in search.py
+        self.initial = city
 
-    def __init__(self, initial, goal, graph):
-        Problem.__init__(self, initial, goal)
-        self.graph = graph
+    """ Swap the position of two cities at random. """
 
-    def actions(self, A):
-        """The actions at a graph node are just its neighbors."""
-        return list(self.graph.get(A).keys())
+    def actions(self, state):
+        actions = []
+        c = state
+
+        # Does the actual city swapping.
+        key1, key2 = random.sample(list(c), 2)
+        c[key1], c[key2] = c[key2], c[key1]
+
+        actions.append(c)
+        return actions
+
+    """Makes the given move on a copy of the given state."""
 
     def result(self, state, action):
-        """The result of going to a neighbor is just that neighbor."""
-        return action
+        new_state = state[:]
+        new_state = action
+        return new_state
 
-    def path_cost(self, cost_so_far, A, action, B):
-        return cost_so_far + (self.graph.get(A, B) or search.infinity)
+    """ Compute the total distance of the circuit. """
 
-    def find_min_edge(self):
-        """Find minimum value of edges."""
-        m = search.infinity
-        for d in self.graph.graph_dict.values():
-            local_min = min(d.values())
-            m = min(m, local_min)
+    def value(self, state):
 
-        return m
+        # Pseudo-code.
+        # Take the state (list of cities in its current order)
+        # Determine the pairs of cities in the circuit from start to end.
+        # Refer to defined distances between pairs of cities in dictionary.
+        # Calculate the distance traveled for this circuit.
+        # Return the result.
 
-    def h(self, node):
-        """h function is straight-line distance from a node's state to goal."""
-        locs = getattr(self.graph, 'locations', None)
-        if locs:
-            if type(node) is str:
-                return int(distance(locs[node], locs[self.goal]))
-
-            return int(distance(locs[node.state], locs[self.goal]))
-        else:
-            return search.infinity
+        definedDistances = self.distances
+        totalDistance = 0
 
 
-# Executes the problem and finds a solution, if any.
+
+
+        return totalDistance
+
+
+################################################################################################
+################################################################################################
+
+# Utility function to generate a random number to be used as distance between cities.
+def get_rng():
+    return random.randint(1, 100)
+
+
+""" Attempt to solve a sample Traveling Salesman Problem and print solution to console. """
+
 if __name__ == '__main__':
 
-    # Generate a random number to be used as distance between cities.
-    randomInt = random.randint(0, 100)
+    # Create simple list of cities to travel to.
+    cities = {'City1', 'City2', 'City3', 'City4'}
 
-    # Create simple map of cities to travel to.
-    cities_map = search.UndirectedGraph(dict(
-        CityA=dict(CityB=randomInt, CityC=randomInt, CityD=randomInt),
-        CityB=dict(CityC=randomInt, CityD=randomInt),
-        CityC=dict(CityD=randomInt)))
+    # Debug statement.
+    print('My cities:    ' + str(cities))
 
-    # Debug. - need to add/find methods to print out nodes and connections.
-    print("Map: " + str(cities_map))
+    # Create dictionary of distances between all possible pairs of cities.
+    cityDistances = {}
+    tuples = {}
+    for c1 in cities:
+        for c2 in cities:
+            if c1 != c2:
+                cityDistances[(c1, c2)] = get_rng()
+
+    # Debug statement.
+    for key, value in cityDistances.items():
+        print("Key is: " + str(key))
+        print("Value is: " + str(value))
 
     # Initialize the Traveling Salesman Problem.
-    travel = Salesman(cities_map.nodes[0], cities_map.nodes[0], cities_map)
+    travel = Salesman(cityDistances, cities)
 
     # Solve the problem using hill climbing.
+    hillStartTime = time.time()
     hill_solution = hill_climbing(travel)
+    hillEndTime = time.time()
     print('Hill-climbing:')
     print('\tSolution: ' + str(hill_solution))
     print('\tValue:    ' + str(travel.value(hill_solution)))
     print('\tGoal?     ' + str(travel.goal_test(hill_solution)))
+    print('\tTime to find solution using hill-climbing' + str(hillEndTime - hillStartTime))
 
     # Solve the problem using simulated annealing.
-    annealing_solution = simulated_annealing(
-        p,
-        exp_schedule(k=20, lam=0.005, limit=10000)
-    )
+    annealStartTime = time.time()
+    annealing_solution = simulated_annealing(travel,
+                                             exp_schedule(k=20, lam=0.005, limit=10000))
+    annealEndTime = time.time()
     print('Simulated annealing:')
     print('\tSolution: ' + str(annealing_solution))
     print('\tValue:    ' + str(travel.value(annealing_solution)))
     print('\tGoal?     ' + str(travel.goal_test(annealing_solution)))
+    print('\tTime to find solution using simulated annealing' + str(annealEndTime - annealStartTime))
+
+    ################################################################################################
+    ################################################################################################
