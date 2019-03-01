35c3a7d4ef1f9aec3a33bf816a37ad637f3a49a6
Joseph Jinn <sorcere666@gmail.com>
2019-02-28 22:47:25 -0500

    Bayesian network code finished for HW 02.

--- Homeworks/Homework2/bayesian_network.py
+++ Homeworks/Homework2/bayesian_network.py
@@ -12,65 +12,72 @@ from probability import BayesNet, enumeration_ask, elimination_ask, gibbs_ask, r
 T, F = True, False
 
 # From AIMA code (probability.py) - Fig. 14.2 - burglary example
-burglary = BayesNet([
-    ('Burglary', '', 0.001),
-    ('Earthquake', '', 0.002),
-    ('Alarm', 'Burglary Earthquake', {(T, T): 0.95, (T, F): 0.94, (F, T): 0.29, (F, F): 0.001}),
-    ('JohnCalls', 'Alarm', {T: 0.90, F: 0.05}),
-    ('MaryCalls', 'Alarm', {T: 0.70, F: 0.01})
+cloudy = BayesNet([
+    ('Cloudy', '', 0.50),
+    ('Rain', 'Cloudy', {T: 0.80, F: 0.20}),
+    ('Sprinkler', 'Cloudy', {T: 0.10, F: 0.50}),
+    ('WetGrass', 'Sprinkler Rain', {(T, T): 0.99, (T, F): 0.90, (F, T): 0.90, (F, F): 0.00})
     ])
 
-# Compute P(Burglary | John and Mary both call).
-print("\nP(Burglary | John and Mary both call)")
-print(enumeration_ask('Burglary', dict(JohnCalls=T, MaryCalls=T), burglary).show_approx())
+# Compute P(Cloudy)
+print("\nP(Cloudy)")
+print(enumeration_ask('Cloudy', dict(), cloudy).show_approx())
 # elimination_ask() is a dynamic programming version of enumeration_ask().
-print(elimination_ask('Burglary', dict(JohnCalls=T, MaryCalls=T), burglary).show_approx())
+print(elimination_ask('Cloudy', dict(), cloudy).show_approx())
 # gibbs_ask() is an approximation algorithm helps Bayesian Networks scale up.
-print(gibbs_ask('Burglary', dict(JohnCalls=T, MaryCalls=T), burglary).show_approx())
+print(gibbs_ask('Cloudy', dict(), cloudy).show_approx())
 # See the explanation of the algorithms in AIMA Section 14.4.
-print(rejection_sampling('Burglary', dict(JohnCalls=T, MaryCalls=T), burglary).show_approx())
-print(likelihood_weighting('Burglary', dict(JohnCalls=T, MaryCalls=T), burglary).show_approx())
+print(rejection_sampling('Cloudy', dict(), cloudy).show_approx())
+print(likelihood_weighting('Cloudy', dict(), cloudy).show_approx())
 
-# Compute P(Alarm | burglary ∧ ¬earthquake)
-print("\nP(Alarm | burglary ∧ ¬earthquake")
-print(enumeration_ask('Alarm', dict(Burglary=T, Earthquake=F), burglary).show_approx())
-# elimination_ask() is a dynamic programming version of enumeration_ask().
-print(elimination_ask('Alarm', dict(Burglary=T, Earthquake=F), burglary).show_approx())
-# gibbs_ask() is an approximation algorithm helps Bayesian Networks scale up.
-print(gibbs_ask('Alarm', dict(Burglary=T, Earthquake=F), burglary).show_approx())
-# See the explanation of the algorithms in AIMA Section 14.4.
-print(rejection_sampling('Alarm', dict(Burglary=T, Earthquake=F), burglary).show_approx())
-print(likelihood_weighting('Alarm', dict(Burglary=T, Earthquake=F), burglary).show_approx())
+###################################################################################################
 
-# Compute P(John | burglary ∧ ¬earthquake)
-print("\nP(John | burglary ∧ ¬earthquake")
-print(enumeration_ask('JohnCalls', dict(Burglary=T, Earthquake=F), burglary).show_approx())
+# Compute P(Sprinkler | cloudy)
+print("\nP(Sprinkler | cloudy)")
+print(enumeration_ask('Sprinkler', dict(Cloudy=T), cloudy).show_approx())
 # elimination_ask() is a dynamic programming version of enumeration_ask().
-print(elimination_ask('JohnCalls', dict(Burglary=T, Earthquake=F), burglary).show_approx())
+print(elimination_ask('Sprinkler', dict(Cloudy=T), cloudy).show_approx())
 # gibbs_ask() is an approximation algorithm helps Bayesian Networks scale up.
-print(gibbs_ask('JohnCalls', dict(Burglary=T, Earthquake=F), burglary).show_approx())
+print(gibbs_ask('Sprinkler', dict(Cloudy=T), cloudy).show_approx())
 # See the explanation of the algorithms in AIMA Section 14.4.
-print(rejection_sampling('JohnCalls', dict(Burglary=T, Earthquake=F), burglary).show_approx())
-print(likelihood_weighting('JohnCalls', dict(Burglary=T, Earthquake=F), burglary).show_approx())
+print(rejection_sampling('Sprinkler', dict(Cloudy=T), cloudy).show_approx())
+print(likelihood_weighting('Sprinkler', dict(Cloudy=T), cloudy).show_approx())
 
-# Compute P(Burglary | alarm)
-print("\nP(Burglary | alarm)")
-print(enumeration_ask('Burglary', dict(Alarm=T), burglary).show_approx())
-# elimination_ask() is a dynamic programming version of enumeration_ask().
-print(elimination_ask('Burglary', dict(Alarm=T), burglary).show_approx())
-# gibbs_ask() is an approximation algorithm helps Bayesian Networks scale up.
-print(gibbs_ask('Burglary', dict(Alarm=T), burglary).show_approx())
-# See the explanation of the algorithms in AIMA Section 14.4.
-print(rejection_sampling('Burglary', dict(Alarm=T), burglary).show_approx())
-print(likelihood_weighting('Burglary', dict(Alarm=T), burglary).show_approx())
+###################################################################################################
 
-# Compute P(Burglary | john ∧ mary)
-print("\nP(Burglary | john ∧ mary")
-print(enumeration_ask('Burglary', dict(JohnCalls=T, MaryCalls=T), burglary).show_approx())
+# Compute P(Cloudy| the sprinkler is running and it’s not raining)
+print("\nP(Cloudy| the sprinkler is running and it’s not raining)")
+print(enumeration_ask('Cloudy', dict(Sprinkler=T, Rain=F), cloudy).show_approx())
 # elimination_ask() is a dynamic programming version of enumeration_ask().
-print(elimination_ask('Burglary', dict(JohnCalls=T, MaryCalls=T), burglary).show_approx())
+print(elimination_ask('Cloudy', dict(Sprinkler=T, Rain=F), cloudy).show_approx())
 # gibbs_ask() is an approximation algorithm helps Bayesian Networks scale up.
-print(gibbs_ask('Burglary', dict(JohnCalls=T, MaryCalls=T), burglary).show_approx())
+print(gibbs_ask('Cloudy', dict(Sprinkler=T, Rain=F), cloudy).show_approx())
 # See the explanation of the algorithms in AIMA Section 14.4.
-print(rejection_sampling('Burglary', dict(JohnCalls=T, MaryCalls=T), burglary).show_approx())
-print(likelihood_weighting('Burglary', dict(JohnCalls=T, MaryCalls=T), burglary).show_approx())
+print(rejection_sampling('Cloudy', dict(Sprinkler=T, Rain=F), cloudy).show_approx())
+print(likelihood_weighting('Cloudy', dict(Sprinkler=T, Rain=F), cloudy).show_approx())
+
+###################################################################################################
+
+# Compute P(WetGrass | it’s cloudy, the sprinkler is running and it’s raining)
+print("\nP(WetGrass | it’s cloudy, the sprinkler is running and it’s raining)")
+print(enumeration_ask('WetGrass', dict(Cloudy=T, Sprinkler=T, Rain=T), cloudy).show_approx())
+# elimination_ask() is a dynamic programming version of enumeration_ask().
+print(elimination_ask('WetGrass', dict(Cloudy=T, Sprinkler=T, Rain=T), cloudy).show_approx())
+# gibbs_ask() is an approximation algorithm helps Bayesian Networks scale up.
+print(gibbs_ask('WetGrass', dict(Cloudy=T, Sprinkler=T, Rain=T), cloudy).show_approx())
+# See the explanation of the algorithms in AIMA Section 14.4.
+print(rejection_sampling('WetGrass', dict(Cloudy=T, Sprinkler=T, Rain=T), cloudy).show_approx())
+print(likelihood_weighting('WetGrass', dict(Cloudy=T, Sprinkler=T, Rain=T), cloudy).show_approx())
+
+###################################################################################################
+
+# Compute P(Cloudy | the grass is not wet)
+print("\nP(Cloudy | the grass is not wet)")
+print(enumeration_ask('Cloudy', dict(WetGrass=F), cloudy).show_approx())
+# elimination_ask() is a dynamic programming version of enumeration_ask().
+print(elimination_ask('Cloudy', dict(WetGrass=F), cloudy).show_approx())
+# gibbs_ask() is an approximation algorithm helps Bayesian Networks scale up.
+print(gibbs_ask('Cloudy', dict(WetGrass=F), cloudy).show_approx())
+# See the explanation of the algorithms in AIMA Section 14.4.
+print(rejection_sampling('Cloudy', dict(WetGrass=F), cloudy).show_approx())
+print(likelihood_weighting('Cloudy', dict(WetGrass=F), cloudy).show_approx())
