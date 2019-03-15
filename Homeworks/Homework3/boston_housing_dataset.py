c9290d6a69180d8805fba48844b0a2ff5284c431
Joseph Jinn <sorcere666@gmail.com>
2019-03-14 21:31:18 -0400

    homework 03 - template setup.

---
+++ Homeworks/Homework3/boston_housing_dataset.py
@@ -0,0 +1,62 @@
+"""
+Course: CS 344 - Artificial Intelligence
+Instructor: Professor VanderLinden
+Name: Joseph Jinn
+Date: 3-15-19
+
+Homework 3 - Regression
+Boston Housing Dataset - Load and Manipulate via Tensorflow
+
+Notes:
+
+TODO - add notes.
+
+############################################
+
+Resources Used:
+
+URL: https://colab.research.google.com/notebooks/mlcc/intro_to_pandas.ipynb
+(intro to Pandas)
+
+URL: https://developers.google.com/machine-learning/crash-course/first-steps-with-tensorflow/video-lecture
+(machine-learning crash course)
+
+URL: https://matplotlib.org/tutorials/introductory/usage.html
+(intro to matplotlib)
+
+URL: https://docs.scipy.org/doc/numpy/user/quickstart.html
+(numpy tutorial)
+
+URL: https://www.tensorflow.org/guide/keras
+(tensorflow - guide to keras)
+
+URL: https://www.tensorflow.org/guide/using_gpu
+(tensorflow - using CUDA)
+
+############################################
+
+Assignment Instructions:
+
+Use Python/NumPy/Pandas/Keras to load and manipulate the Boston Housing Dataset as follows.
+
+Compute the dimensions of the data structures. Include code to print these values.
+
+Construct a suitable testing set, training set, and validation set for this data.
+Include code to create these datasets but do not include the datasets themselves.
+
+Create one new synthetic feature that could be useful for machine learning in this domain.
+Explain what it is and why it might be useful.
+
+"""
+
+############################################################################################
+
+if __name__ == '__main__':
+    """
+    Executes the program.
+    """
+
+    print("Boston Housing Dataset - Tensorflow Data Manipulation")
+
+############################################################################################
+############################################################################################
