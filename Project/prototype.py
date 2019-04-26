e5dc4b70c7272cdb2426280b62095c6797451777
Joseph Jinn <sorcere666@gmail.com>
2019-04-25 23:02:30 -0400

    Final project - initial SLO dataset import and pre-processing.

--- Project/prototype.py
+++ Project/prototype.py
@@ -18,9 +18,59 @@ TODO - placeholder
 
 ################################################################################################################
 
+# TensorFlow and tf.keras
+import tensorflow as tf
+from tensorflow import keras
 
+from matplotlib import pyplot as plt
+import numpy as np
+import pandas as pd
+
+tf.logging.set_verbosity(tf.logging.ERROR)
+pd.options.display.max_rows = 10
+pd.options.display.float_format = '{:.1f}'.format
+
+################################################################################################################
+
+# Import the dataset.
+slo_dataset = \
+    pd.read_csv("datasets/tbl_training_set.csv", sep=",")
+
+# Shuffle the data randomly.
+slo_dataset = slo_dataset.reindex(
+    np.random.permutation(slo_dataset.index))
+
+# Rename columns to something that makes sense.
+column_names = ['Tweet', 'SLO1', 'SLO2', 'SLO3']
+slo_dataset.columns = column_names
+
+# Print shape and column names.
+print("The shape of our SLO dataset:")
+print(slo_dataset.shape)
+print()
+print("The columns of our SLO dataset:")
+print(slo_dataset.head)
+print()
+
+# Create input features.
+selected_features = slo_dataset[column_names]
+processed_features = selected_features.copy()
+
+print("The tweets as a string:")
+print(processed_features['Tweet'])
+print()
+print("SLO classification tag 1 (if any):")
+print(processed_features['SLO1'])
+print()
+print("SLO classification tag 2 (if any):")
+print(processed_features['SLO2'])
+print()
+print("SLO classification tag 3 (if any):")
+print(processed_features['SLO3'])
+print()
 
 ############################################################################################
+
 """
 Main function.  Execute the program.
 """
@@ -30,5 +80,5 @@ debug = 0
 if __name__ == '__main__':
     print()
 
-    ############################################################################################
+############################################################################################
 
