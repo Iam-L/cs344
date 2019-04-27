bab195782574bd0ff0e0a1790e902463b613c6bd
Joseph Jinn <sorcere666@gmail.com>
2019-04-27 19:32:14 -0400

    SLO TBL classification - working multinomialNB with terrible accuracy. (0.5061728395061729, 0.4444444444444444)

--- Project/slo_topic_classification.py
+++ Project/slo_topic_classification.py
@@ -6,12 +6,16 @@ Date: 4-23-19
 
 Final Project - SLO Topic Classification
 
+###########################################################
 Notes:
 
 Proceeding with provided labeled SLO TBL dataset.  Will attempt to preprocess and train this.
 
 Using the "NLTK" Natural Language Toolkit as replacement for CMU Tweet Tagger preprocessor.
 
+Using a combination of Sci-kit Learn, Numpy/Pandas, Tensorflow/Keras, and matplotlib.
+
+###########################################################
 Resources Used:
 
 https://stackoverflow.com/questions/13413590/how-to-drop-rows-of-pandas-dataframe-whose-value-in-certain-columns-is-nan
@@ -33,10 +37,67 @@ https://stackoverflow.com/questions/23667369/drop-all-duplicate-rows-in-python-p
 (drop duplicate examples)
 
 https://www.nltk.org/
+http://www.nltk.org/book/
 (text pre-processing)
 
 https://stackoverflow.com/questions/34784004/python-text-processing-nltk-and-pandas
+https://stackoverflow.com/questions/48049087/nltk-based-text-processing-with-pandas
+https://stackoverflow.com/questions/44173624/how-to-apply-nltk-word-tokenize-library-on-a-pandas-dataframe-for-twitter-data
 (tokenize tweets using pands and nltk)
+
+https://www.dataquest.io/blog/settingwithcopywarning/
+(SettingWithCopyWarning explanation)
+
+https://stackoverflow.com/questions/42750551/converting-a-string-to-a-lower-case-pandas
+(down-case all text)
+
+https://stackoverflow.com/questions/20490274/how-to-reset-index-in-a-pandas-data-frame
+(reindex the dataframe)
+
+###########################################################
+Regular expressions section:
+
+https://stackoverflow.com/questions/11331982/how-to-remove-any-url-within-a-string-in-python/40823105#40823105
+(remove URL's)
+
+https://stackoverflow.com/questions/8376691/how-to-remove-hashtag-user-link-of-a-tweet-using-regular-expression
+(remove mentions)
+
+https://www.machinelearningplus.com/python/python-regex-tutorial-examples/
+(remove stuff from tweets via regex)
+
+https://stackoverflow.com/questions/265960/best-way-to-strip-punctuation-from-a-string-in-python
+(remove punctuation from strings)
+
+https://www.w3schools.com/python/python_regex.asp
+(basic tutorial on regular expressions)
+
+###########################################################
+Sci-kit Learn section:
+
+https://www.dataquest.io/blog/sci-kit-learn-tutorial/
+(sci-kit learn tutorial)
+
+https://stackoverflow.com/questions/49806790/iterable-over-raw-text-documents-expected-string-object-received
+(saved my life)
+
+https://scikit-learn.org/stable/modules/generated/sklearn.feature_extraction.text.CountVectorizer.html
+(CounteVectorizer)
+
+https://realpython.com/python-keras-text-classification/
+(text classification tutorial using python, sci-kit learn, and keras)
+
+https://nlpforhackers.io/keras-intro/
+(text classification using keras and NN's)
+
+https://scikit-learn.org/stable/modules/generated/sklearn.preprocessing.LabelEncoder.html
+(encode labels from categorical to numerical)
+
+https://scikit-learn.org/stable/tutorial/text_analytics/working_with_text_data.html
+(followed this one initially for text classification)
+
+https://stackoverflow.com/questions/45804133/dimension-mismatch-error-in-countvectorizer-multinomialnb
+(only call fit_transform() once to fit to the dataset; afterwards, use transform() only otherwise issues)
 
 """
 
@@ -50,6 +111,8 @@ from matplotlib import pyplot as plt
 import numpy as np
 import pandas as pd
 import nltk as nltk
+from nltk.tokenize import TweetTokenizer
+import re
 
 tf.logging.set_verbosity(tf.logging.ERROR)
 pd.options.display.max_rows = 10
@@ -255,10 +318,82 @@ if debug:
               + str(slo_dataframe_TBL_duplicates_dropped['SLO'][index]))
     print('Shape of dataframes without duplicate TBL values: ' + str(slo_dataframe_TBL_duplicates_dropped.shape))
 
+
+#######################################################
+
+def preprocess_tweets(tweet_text):
+    """
+    Function performs NLTK text pre-processing.
+
+    Notes:
+
+    Stop words are retained.
+
+    TODO - shrink character elongations
+    TODO - remove non-english tweets
+    TODO - remove non-company associated tweets
+    TODO - remove year and time.
+    TODO - remove cash items?
+
+    :return:
+    """
+
+    # Remove "RT" tags.
+    preprocessed_tweet_text = re.sub("rt", "", tweet_text)
+
+    # Remove URL's.
+    preprocessed_tweet_text = re.sub("http[s]?://\S+", "slo_url", preprocessed_tweet_text)
+
+    # Remove Tweet mentions.
+    preprocessed_tweet_text = re.sub("@\S+", "slo_mention", preprocessed_tweet_text)
+
+    # Remove Tweet hashtags.
+    preprocessed_tweet_text = re.sub("#\S+", "slo_hashtag", preprocessed_tweet_text)
+
+    # Remove all punctuation.
+    # preprocessed_tweet_text = preprocessed_tweet_text.translate(str.maketrans('', '', string.punctuation))
+
+    return preprocessed_tweet_text
+
+
+# Assign new dataframe to contents of old.
+slo_df_tokenized = slo_dataframe_TBL_duplicates_dropped
+
+# Down-case all text.
+slo_df_tokenized['Tweet'] = slo_df_tokenized['Tweet'].str.lower()
+
+# Pre-process each tweet individually.
+for index in slo_df_tokenized.index:
+    slo_df_tokenized['Tweet'][index] = preprocess_tweets(slo_df_tokenized['Tweet'][index])
+
+################################################################################################################
+
+# # Use NLTK to tokenize each Tweet.
+# tweet_tokenizer = TweetTokenizer()
+# slo_df_tokenized['Tweet'] = slo_dataframe_TBL_duplicates_dropped['Tweet'].apply(tweet_tokenizer.tokenize)
+
+# Use for NLTK debugging.
+# if debug:
+#     print('\n')
+#     print("SLO TBL dataframe tokenized:")
+#     # Iterate through each row and check that we no longer have examples with NaN values.
+#     for index in slo_df_tokenized.index:
+#         print(slo_df_tokenized['Tweet'][index])
+#     print('Shape of tokenized dataframe: ' + str(slo_df_tokenized.shape))
+
+# for index in slo_df_tokenized.index:
+#     slo_df_tokenized['Tweet'][index] = vectorizer.transform(slo_df_tokenized['Tweet'][index]).toarray()
+
+################################################################################################################
+
+# Reindex everything.
+slo_df_tokenized.index = pd.RangeIndex(len(slo_df_tokenized.index))
+# slo_df_tokenized.index = range(len(slo_df_tokenized.index))
+
 ################################################################################################################
 
 # Create input features.
-selected_features = slo_dataframe_TBL_duplicates_dropped[column_names_single]
+selected_features = slo_df_tokenized[column_names_single]
 processed_features = selected_features.copy()
 
 # Check what we are using for input features.
@@ -269,6 +404,203 @@ if debug:
     print()
     print("SLO TBL classification:")
     print(processed_features['SLO'])
+
+# Create feature and target sets.
+slo_feature_input = processed_features['Tweet']
+slo_targets = processed_features['SLO']
+
+# Create training and test sets.
+from sklearn.model_selection import train_test_split
+
+# Note: these are no longer Pandas format, they're Sci-kit Learn format.
+tweet_train, tweet_test, target_train, target_test = train_test_split(slo_feature_input, slo_targets, test_size=0.33,
+                                                                      random_state=42)
+
+if debug:
+    print("Shape of tweet training set:")
+    print(tweet_train.data.shape)
+    print("Shape of tweet test set:")
+    print(tweet_test.data.shape)
+    print("Shape of target training set:")
+    print(target_train.data.shape)
+    print("Shape of target test set:")
+    print(target_test.data.shape)
+
+#######################################################
+
+# Use Sci-kit learn to encode labels into integer values - one assigned integer value per class.
+from sklearn import preprocessing
+
+target_label_encoder = preprocessing.LabelEncoder()
+
+target_train_encoded = target_label_encoder.fit_transform(target_train)
+target_test_encoded = target_label_encoder.fit_transform(target_test)
+target_train_DEcoded = target_label_encoder.inverse_transform(target_train_encoded)
+target_test_DEcoded = target_label_encoder.inverse_transform(target_test_encoded)
+
+if debug:
+    print("Encoded target training labels:")
+    print(target_train_encoded)
+    print("Decoded target training labels:")
+    print(target_train_DEcoded)
+
+    print("Encoded target test labels:")
+    print(target_test_encoded)
+    print("Decoded target test labels:")
+    print(target_test_DEcoded)
+
+#######################################################
+
+# Use Sci-kit learn to tokenize each Tweet.
+from sklearn.feature_extraction.text import CountVectorizer
+
+vectorizer = CountVectorizer(min_df=0, lowercase=False)
+tweet_train_encoded = vectorizer.fit_transform(tweet_train)
+tweet_test_encoded = vectorizer.transform(tweet_test)
+
+if debug:
+    print("Vectorized tweet training set:")
+    print(tweet_train_encoded)
+    print("Vectorized tweet testing set:")
+    print(tweet_test_encoded)
+    print("Shape of the tweet training set:")
+    print(tweet_train_encoded.shape)
+    print("Shape of the tweet testing set:")
+    print(tweet_test_encoded.shape)
+
+from sklearn.feature_extraction.text import TfidfTransformer
+
+tfidf_transformer = TfidfTransformer()
+
+tweet_train_encoded_tfidf = tfidf_transformer.fit_transform(tweet_train_encoded)
+tweet_test_encoded_tfidf = tfidf_transformer.transform(tweet_test_encoded)
+
+if debug:
+    print("vectorized tweet training set term frequencies down-sampled:")
+    print(tweet_train_encoded_tfidf)
+    print("Shape of the tweet training set term frequencies: ")
+    print(tweet_train_encoded_tfidf.shape)
+    print("vectorized tweet test set term frequencies down-sampled:")
+    print(tweet_test_encoded_tfidf)
+    print("Shape of the tweet test set term frequencies: ")
+    print(tweet_test_encoded_tfidf.shape)
+
+################################################################################################################
+"""
+Train the model using a variety of different classifiers.
+"""
+
+from sklearn.naive_bayes import MultinomialNB
+
+clf_multinomialNB = MultinomialNB().fit(tweet_train_encoded_tfidf, target_train_encoded)
+
+# from sklearn.svm import LinearSVC
+# from sklearn.metrics import accuracy_score
+#
+# # create an object of type LinearSVC
+# svc_model = LinearSVC(random_state=0)
+#
+# # train the algorithm on training data and predict using the testing data
+# pred = svc_model.fit(tweet_train, target_train).predict(tweet_test)
+#
+# # print the accuracy score of the model
+# print("LinearSVC accuracy : ", accuracy_score(target_test, pred, normalize=True))
+
+################################################################################################################
+"""
+Make predictions using pre-processed and tokenized Tweets from CMU Tweet Tagger.
+Note: This required .csv import and vectorization.
+
+Probably won't be the best generalization to new data as the vocabulary between these two different datasets could
+be drastically different.
+
+"""
+# Import the dataset.
+slo_dataset_cmu = \
+    pd.read_csv("borg-SLO classifiers/dataset_20100101-20180510_tok.csv", sep=",")
+
+# Shuffle the data randomly.
+slo_dataset_cmu = slo_dataset_cmu.reindex(
+    np.random.permutation(slo_dataset_cmu.index))
+
+# Generate a Pandas dataframe.
+slo_dataframe_cmu = pd.DataFrame(slo_dataset_cmu)
+
+# Print shape and column names.
+print()
+print("The shape of our SLO CMU dataframe:")
+print(slo_dataframe_cmu.shape)
+print()
+print("The columns of our SLO CMU dataframe:")
+print(slo_dataframe_cmu.head)
+print()
+
+# Create input features.
+selected_features_cmu = slo_dataframe_cmu['tweet_t']
+processed_features_cmu = selected_features_cmu.copy()
+
+# Check what we are using for predictions.
+if debug:
+    print("The shape of our SLO CMU feature dataframe:")
+    print(slo_dataframe_cmu.shape)
+    print()
+    print("The columns of our SLO CMU feature dataframe:")
+    print(processed_features_cmu.head)
+    print()
+
+#######################################################
+
+# Vectorize the categorical data for use in predictions.
+tweet_predict_encoded = vectorizer.transform(processed_features_cmu)
+
+if debug:
+    print("Vectorized tweet predictions set:")
+    print(tweet_predict_encoded)
+    print("Shape of the tweet predictions set:")
+    print(tweet_predict_encoded.shape)
+    print()
+
+tweet_predict_encoded_tfidf = tfidf_transformer.transform(tweet_predict_encoded)
+
+if debug:
+    print("vectorized tweet predictions set term frequencies down-sampled:")
+    print(tweet_predict_encoded_tfidf)
+    print("Shape of the tweet predictions set term frequencies: ")
+    print(tweet_predict_encoded_tfidf.shape)
+    print()
+
+# Generalize to new data and predict.
+tweet_generalize_new_data_predictions = clf_multinomialNB.predict(tweet_predict_encoded_tfidf)
+
+# View the results.
+# for doc, category in zip(processed_features_cmu, tweet_generalize_new_data_predictions):
+#     print('%r => %s' % (doc, category))
+
+################################################################################################################
+
+# Predict using test dataset.
+tweet_test_predictions = clf_multinomialNB.predict(tweet_test_encoded_tfidf)
+
+# View the results.
+for doc, category in zip(tweet_test, tweet_test_predictions):
+    print('%r => %s' % (doc, category))
+
+# Measure accuracy.
+print()
+print("Accuracy for test set predictions using multinomialNB:")
+print(str(np.mean(tweet_test_predictions == target_test_encoded)))
+
+################################################################################################################
+"""
+Build pipeline for the future.
+"""
+from sklearn.pipeline import Pipeline
+
+text_clf = Pipeline([
+    ('vect', CountVectorizer()),
+    ('tfidf', TfidfTransformer()),
+    ('multinomialNB', MultinomialNB()),
+])
 
 ############################################################################################
 
