3828dbfaf48df4f7bbff591147573fed8a9cab0b
Joseph Jinn <sorcere666@gmail.com>
2019-04-27 00:05:23 -0400

    pre-processing: removed all Tweets without TBL classification labels.

--- Project/slo_topic_classification.py
+++ Project/slo_topic_classification.py
@@ -30,6 +30,7 @@ tf.logging.set_verbosity(tf.logging.ERROR)
 pd.options.display.max_rows = 10
 pd.options.display.float_format = '{:.1f}'.format
 
+debug = True
 ################################################################################################################
 
 # Import the dataset.
@@ -42,20 +43,45 @@ slo_dataset = slo_dataset.reindex(
 
 # Rename columns to something that makes sense.
 column_names = ['Tweet', 'SLO1', 'SLO2', 'SLO3']
-slo_dataset.columns = column_names
+
+# Generate a Pandas dataframe.
+slo_dataframe = pd.DataFrame(slo_dataset)
 
 # Print shape and column names.
 print("The shape of our SLO dataset:")
-print(slo_dataset.shape)
+print(slo_dataframe.shape)
 print()
 print("The columns of our SLO dataset:")
-print(slo_dataset.head)
+print(slo_dataframe.head)
 print()
 
+# Assign column names.
+slo_dataframe.columns = column_names
+
+# Data pre-processing
+# TODO - use pre-processing methods indicated in SLO article
+# TODO - https://github.com/Calvin-CS/slo-classifiers/tree/feature/keras-nn/stance/data
+# TODO - https://github.com/Calvin-CS/slo-classifiers/blob/feature/keras-nn/stance/data/tweet_preprocessor.py
+# FIXME - preprocessor will only work on Linux/Mac
+
+# Drop all rows with only NaN in all columns.
+slo_dataframe = slo_dataframe.dropna(how='all')
+# Drop all rows without at least 2 non NaN values - indicating no SLO classification labels.
+slo_dataframe = slo_dataframe.dropna(thresh=2)
+print(slo_dataframe.shape)
+print()
+
+if debug:
+    # Iterate through each row.
+    for index in slo_dataframe.index:
+        print(slo_dataframe['Tweet'][index] + '\tSLO1: ' + str(slo_dataframe['SLO1'][index])
+              + '\tSLO2: ' + str(slo_dataframe['SLO2'][index]) + '\tSLO3: ' + str(slo_dataframe['SLO3'][index]))
+
 # Create input features.
-selected_features = slo_dataset[column_names]
+selected_features = slo_dataframe[column_names]
 processed_features = selected_features.copy()
 
+print()
 print("The tweets as a string:")
 print(processed_features['Tweet'])
 print()
@@ -69,13 +95,6 @@ print("SLO classification tag 3 (if any):")
 print(processed_features['SLO3'])
 print()
 
-# Data pre-processing
-# TODO - use pre-processing methods indicated in SLO article
-# TODO - can I get the code that does the pre-processing and appropriate it for my own use here?
-
-# TODO - https://github.com/Calvin-CS/slo-classifiers/tree/feature/keras-nn/stance/data
-# TODO - https://github.com/Calvin-CS/slo-classifiers/blob/feature/keras-nn/stance/data/tweet_preprocessor.py
-
 ############################################################################################
 
 """
@@ -88,4 +107,3 @@ if __name__ == '__main__':
     print()
 
 ############################################################################################
-
